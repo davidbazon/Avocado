c89d54091c28006d01dbc7f8623054f3c953cade
davidbazon <dutzu.bazon@gmail.com>
2023-12-16 14:40:23 +0200

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parts/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Parts/MecanumDrive.java
@@ -7,22 +7,22 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class MecanumDrive {
-   private DcMotor TopLeftMotor;
-   private DcMotor TopRightMotor;
-   private DcMotor BottomLeftMotor;
-   private DcMotor BottomRightMotor;
+   private DcMotor topLeftMotor;
+   private DcMotor topRightMotor;
+   private DcMotor bottomLeftMotor;
+   private DcMotor bottomRightMotor;
 
 
    @SuppressLint("NotConstructor")
    public void MecanumDrive(HardwareMap hm){  //void Start()
-       TopLeftMotor = hm.get(DcMotor.class, "tlm");
-       TopRightMotor = hm.get(DcMotor.class, "trm");
-       BottomLeftMotor = hm.get(DcMotor.class, "blm");
-       BottomRightMotor = hm.get(DcMotor.class, "brm");
+       topLeftMotor = hm.get(DcMotor.class, "tlm");
+       topRightMotor = hm.get(DcMotor.class, "trm");
+       bottomLeftMotor = hm.get(DcMotor.class, "blm");
+       bottomRightMotor = hm.get(DcMotor.class, "brm");
 
-       TopLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-       BottomLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+       topLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+       bottomLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-       TopLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+       topLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
    }
 }
